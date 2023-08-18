1ace4965c82b21a45efc8e183e4b0e46127072c8
MennaMoataz23 <mennamansour297@gmail.com>
2023-08-18 16:47:05 +0300

    Book & Author added with their repos, servcies and controllers

--- src/main/java/com/example/bookStore/model/entities/User.java
+++ src/main/java/com/example/bookStore/model/entities/User.java
@@ -1,67 +1,67 @@
-package com.example.bookStore.model.entities;
-
-import com.example.bookStore.model.constants.enums.Role;
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table
-public class User implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @Column(name = "userName", nullable = false)
-    private String userName;
-
-    @Column(name = "email", nullable = false)
-    private String email;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-
-    private Role role = Role.USER;
-
-    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    private List<Book> bookList;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-}
+//package com.example.bookStore.model.entities;
+//
+//import com.example.bookStore.model.constants.enums.Role;
+//import lombok.*;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.List;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//@Table
+//public class User implements UserDetails {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    @Column(name = "userName", nullable = false)
+//    private String userName;
+//
+//    @Column(name = "email", nullable = false)
+//    private String email;
+//
+//    @Column(name = "password", nullable = false)
+//    private String password;
+//
+//    private Role role = Role.USER;
+//
+//    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+//    private List<Book> bookList;
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return false;
+//    }
+//}
